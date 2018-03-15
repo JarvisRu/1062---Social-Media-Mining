7333f2e7d12dbd0410650c796f65e96cb642fa81
JarvisRu <dar58896123@gmail.com>
2018-03-15 20:17:29 +0800

    first commit
    DecisionTree predict & accuracy done

---
+++ HW1/decisionTree.py
@@ -0,0 +1,25 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection  import train_test_split
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+import pandas as pd
+import numpy as np
+
+# load iris dataset
+iris = load_iris()
+iris_x = iris.data
+iris_y = iris.target
+
+# split train & test
+train_x, test_x, train_y, test_y = train_test_split(iris_x, iris_y, test_size=.3)
+
+# create classification
+clf = tree.DecisionTreeClassifier()
+iris_clf = clf.fit(train_x, train_y)
+
+# predict with test_x
+predicted_y = iris_clf.predict(test_x)
+
+# get the accuracy
+dc_accuracy = accuracy_score(test_y, predicted_y)
+print(dc_accuracy)
