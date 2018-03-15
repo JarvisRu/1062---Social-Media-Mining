089e93141ae881b664a0fcc5669b3a88bd33dc9a
JarvisRu <dar58896123@gmail.com>
2018-03-15 23:00:08 +0800

    KNN - find the best k KNN_figure - for hw1 & is still need to work on

--- HW1/KNN_figure.py
+++ HW1/KNN_figure.py
@@ -3,6 +3,7 @@ from sklearn.model_selection  import train_test_split
 from sklearn import neighbors
 from sklearn.metrics import accuracy_score
 import numpy as np
+from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
 
 # load iris dataset
@@ -15,23 +16,6 @@ iris_y = iris.target
 # split train & test
 train_x, test_x, train_y, test_y = train_test_split(iris_x, iris_y, test_size=.27)
 
-# # find the best k
-# k_range = np.arange(1, round(train_x.shape[0] * 0.2))
-# accuarcies = []
-# for k in k_range:
-#     clf = neighbors.KNeighborsClassifier(k)
-#     iris_clf = clf.fit(train_x, train_y)
-#     predicted_y = iris_clf.predict(test_x)
-#     knn_accuracy = accuracy_score(test_y, predicted_y)
-#     accuarcies.append(knn_accuracy)
-#     print(k, ":", knn_accuracy)
-
-# # print the scatter of k
-# print(max(accuarcies))
-# plt.scatter(k_range, accuarcies)
-# plt.show()
-
-
 # create classification - KNN
 clf = neighbors.KNeighborsClassifier(8)
 iris_clf = clf.fit(train_x, train_y)
@@ -39,6 +23,16 @@ iris_clf = clf.fit(train_x, train_y)
 # predict with test_x
 predicted_y = iris_clf.predict(test_x)
 
-# get the accuracy
-knn_accuracy = accuracy_score(test_y, predicted_y)
-print(knn_accuracy)
+# get x[0] and x[1] limit
+x0_min, x0_max = iris_x[:, 0].min()-1, iris_x[:,0].max()+1 
+x1_min, x1_max = iris_x[:, 1].min()-1, iris_x[:,1].max()+1
+
+
+# generate meshgrid and cmap for figure
+xx, yy  = np.meshgrid(np.arange(x0_min, x0_max, .02),np.arange(x1_min, x1_max, .02))
+cmad_background = ListedColormap(['red','green','blue'])
+
+
+# # get the accuracy
+# knn_accuracy = accuracy_score(test_y, predicted_y)
+# print(knn_accuracy)
