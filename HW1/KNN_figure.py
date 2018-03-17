0a43ebd159d1b36fcc8e09b715557b0e85054268
JarvisRu <dar58896123@gmail.com>
2018-03-17 15:39:55 +0800

    KNN-figure - almost done

--- HW1/KNN_figure.py
+++ HW1/KNN_figure.py
@@ -5,6 +5,7 @@ from sklearn.metrics import accuracy_score
 import numpy as np
 from matplotlib.colors import ListedColormap
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 # load iris dataset
 iris = load_iris()
@@ -27,12 +28,30 @@ predicted_y = iris_clf.predict(test_x)
 x0_min, x0_max = iris_x[:, 0].min()-1, iris_x[:,0].max()+1 
 x1_min, x1_max = iris_x[:, 1].min()-1, iris_x[:,1].max()+1
 
-
-# generate meshgrid and cmap for figure
+# generate meshgrid
 xx, yy  = np.meshgrid(np.arange(x0_min, x0_max, .02),np.arange(x1_min, x1_max, .02))
-cmad_background = ListedColormap(['red','green','blue'])
 
+# predict class & reshape it for colormesh
+predicted_z = iris_clf.predict(np.c_[xx.ravel(), yy.ravel()])
+predicted_z = predicted_z.reshape(xx.shape)
 
-# # get the accuracy
-# knn_accuracy = accuracy_score(test_y, predicted_y)
-# print(knn_accuracy)
+# prepare cmap
+cmap_background = ListedColormap(['#97cbff','#adfedc','#ffad86'])
+cmap_scatter = ListedColormap(['#005ab5', '#019858', '#a23400'])
+
+# prepare patches
+blue_patch = mpatches.Patch(color="#97cbff", label="iris_Setosa")
+green_patch = mpatches.Patch(color="#adfedc", label="iris_Versicolour")
+red_patch = mpatches.Patch(color="#ffad86", label="iris_Virginica")
+
+# figure part
+plt.figure()
+plt.pcolormesh(xx, yy, predicted_z, cmap=cmap_background)
+plt.scatter(iris_x[:,0], iris_x[:,1], c=iris_y, cmap=cmap_scatter, marker='*')
+plt.legend(handles= [blue_patch, green_patch, red_patch])
+plt.xlim(xx.min()+.5, xx.max()-.5)
+plt.ylim(yy.min()+.5, yy.max()-.5)
+# get the accuracy
+knn_accuracy = accuracy_score(test_y, predicted_y)
+plt.title("KNN : k=8 / Accuracy={}".format(knn_accuracy))
+plt.show()
