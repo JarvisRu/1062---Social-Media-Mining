90ddfed47a204ab7fbd28642a868a84c3d7d0986
JarvisRu <dar58896123@gmail.com>
2018-03-17 16:59:24 +0800

    KNN - draw plot for all k for KNN

--- HW1/KNN.py
+++ HW1/KNN.py
@@ -16,29 +16,34 @@ iris_y = iris.target
 train_x, test_x, train_y, test_y = train_test_split(iris_x, iris_y, test_size=.27)
 
 # # find the best k
-# k_range = np.arange(1, round(train_x.shape[0] * 0.2))
-# accuarcies = []
-# for k in k_range:
-#     clf = neighbors.KNeighborsClassifier(k)
-#     iris_clf = clf.fit(train_x, train_y)
-#     predicted_y = iris_clf.predict(test_x)
-#     knn_accuracy = accuracy_score(test_y, predicted_y)
-#     accuarcies.append(knn_accuracy)
-#     print(k, ":", knn_accuracy)
+k_range = np.arange(1, round(train_x.shape[0] * 0.2))
+accuarcies = []
+for k in k_range:
+    clf = neighbors.KNeighborsClassifier(k)
+    iris_clf = clf.fit(train_x, train_y)
+    predicted_y = iris_clf.predict(test_x)
+    knn_accuracy = accuracy_score(test_y, predicted_y)
+    accuarcies.append(knn_accuracy)
+    print(" K = ",k, "->", knn_accuracy)
 
-# # print the scatter of k
-# print(max(accuarcies))
-# plt.scatter(k_range, accuarcies)
-# plt.show()
+# print the figure
+print(max(accuarcies))
+plt.plot(k_range, accuarcies, '--o')
+plt.xlabel("Value of K for KNN")
+plt.ylabel("Accuracy")
+plt.title("The Highest Accuracy is {}".format(max(accuarcies)))
+plt.show()
 
 
-# create classification - KNN
-clf = neighbors.KNeighborsClassifier(8)
-iris_clf = clf.fit(train_x, train_y)
 
-# predict with test_x
-predicted_y = iris_clf.predict(test_x)
+# basic predict
+# # create classification - KNN
+# clf = neighbors.KNeighborsClassifier(8)
+# iris_clf = clf.fit(train_x, train_y)
 
-# get the accuracy
-knn_accuracy = accuracy_score(test_y, predicted_y)
-print(knn_accuracy)
+# # predict with test_x
+# predicted_y = iris_clf.predict(test_x)
+
+# # get the accuracy
+# knn_accuracy = accuracy_score(test_y, predicted_y)
+# print(knn_accuracy)
